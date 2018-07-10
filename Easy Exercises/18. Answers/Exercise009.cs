e007f8c7ebd804b709e068b64d3cd69df2b071eb
AceCoderYoda <workingmanforums@outlook.com>
2018-07-10 09:58:55 +0200

    Added Recursion

---
+++ Easy Exercises/18. Answers/Exercise009.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Easy_Exercises
+{
+
+public class Exercise009
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\n\n Recursion : Find the factorial of a given number :");
+        Console.WriteLine("-------------------------------------------------------");
+
+        Console.Write(" Input any positive number : ");
+        int n1 = Convert.ToInt32(Console.ReadLine());
+        long fact = FactorialCalcu(n1);
+        Console.WriteLine(" The factorial of {0} is : {1} ", n1, fact);
+        Console.ReadKey();
+    }
+
+    private static long FactorialCalcu(int n1)
+    {
+        if (n1 == 0)
+        {
+            return 1;
+        }
+        return n1 * FactorialCalcu(n1 - 1);
+    }
+    }
+}
